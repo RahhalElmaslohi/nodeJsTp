75f03b2b17bcf2d48f9b9361ab470a5e6e34eca6
RahhalElmaslohi <rahhalelmaslohi@gmail.com>
2023-09-04 16:23:01 -0400

    creation des fichier de l'application

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const request = require('request');
+const fs = require('fs');
+const { PORT, API_KEY } = require('./config.js'); // Assurez-vous d'importer également API_KEY
+
+
+app.use("/static", express.static(path.resolve(__dirname, 'frontend', 'static')))
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"))
+    
+})
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT", PORT);
+});
